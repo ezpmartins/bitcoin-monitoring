d933182259c94dc2beba89dab1da1181b016ddc3
ezpmartins <enzo.martins@sptech.school>
2025-05-20 11:33:19 -0300

    feat: add api_link

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 
+import requests
+import json
+
 color0 = "#444466"
 color1 = "#feffff"
 color2 ="#6f9fbd"
@@ -20,6 +23,13 @@ top_frame.grid(row=1,column=0)
 
 bottom_frame = Frame(window, width=320,height=300,bg=background, pady=0,padx=0, relief='flat')
 bottom_frame.grid(row=2,column=0,sticky=NW)
+
+api_link = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD%2CEUR%2CAOA%2CBRL'
+
+response = requests.get(api_link)
+data = response.json()
+
+print(data)
 
 image = Image.open('images/ic_bitcoin.png')
 image = image.resize((30,30),Image.Resampling.LANCZOS)
@@ -41,8 +51,8 @@ l_p_euro.place(x=10,y=130)
 l_p_real = Label(bottom_frame, text='R$ 10,000,00', width=14,bg=background, fg=color1,relief=FLAT, anchor='center',font=("Arial 12"))
 l_p_real.place(x=0,y=160)
 
-l_p_libra = Label(bottom_frame, text='£ 10,000,00', width=14,bg=background, fg=color1,relief=FLAT, anchor='center',font=("Arial 12"))
-l_p_libra.place(x=0,y=190)
+l_p_kwanzas = Label(bottom_frame, text='Kz 10,000,00', width=14,bg=background, fg=color1,relief=FLAT, anchor='center',font=("Arial 12"))
+l_p_kwanzas.place(x=0,y=190)
 
 
 
